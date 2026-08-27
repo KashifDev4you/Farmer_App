5b6659484bf6a78001a818e7fc021786f6d26b31
Muhammad Kashif <kashifdev4you@gmail.com>
2026-08-27 13:42:53 +0500

    Update routes_managment.dart

--- lib/routs/routes_managment.dart
+++ lib/routs/routes_managment.dart
@@ -1,9 +1,16 @@
-class Apppage {
-  Apppage._();
+import 'package:get/get.dart';
 
-  static final pages = <Getpages>[
-    Getpage()
+import 'app_routes.dart';
+import 'package:food_app/screens/login.dart';
+import 'package:food_app/screens/profile.dart';
+import 'package:food_app/screens/register.dart';
 
+class AppPages {
+  AppPages._();
 
+  static final pages = <GetPage>[
+    GetPage(name: AppRoutes.check, page: () => const CheckoutScreen()),
+    GetPage(name: AppRoutes.explore, page: () => const ExploreScreen()),
+    GetPage(name: AppRoutes.home, page: () => const HomeScreen()),
   ];
 }
