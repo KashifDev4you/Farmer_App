c8367520f55cd3f036ec2a28a68856adcef91b48
Muhammad Kashif <kashifdev4you@gmail.com>
2026-08-27 13:47:50 +0500

    Refactor ApiConstants class with API endpoints

--- lib/api_consts/api_consts.dart
+++ lib/api_consts/api_consts.dart
@@ -1,10 +1,29 @@
-import 'package:flutter/material.dart';
+class ApiConstants {
+  ApiConstants._();
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+  static const String baseUrl = "https://dummyjson.com";
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  static const int connectTimeoutSeconds = 20;
+
+  // Auth endpoints
+
+
+//   user Model
+  static const String allUsers = "users";
+  static const String newUser = "users/add";
+  static const String userPara = "users/search?q={{firstName}}";
+  static const String userFilter = "users/filter?key=hair.color&value=Black";
+
+//   Product model
+  static const String allPorduct = "products";
+  static const String productCategories = 'products/categories';
+  static const String productFromCategores = "products/category/furniture";
+  static const String newProduct = "products/add";
+  static const String updateProduct = "products/1";
+  static const String deleteProduct = "products/1";
+
+//   static const String refreshToken = "/refresh-token";
+
+  // Example authenticated endpoint (for future modules)
+  static const String userProfile = "/users";
 }
